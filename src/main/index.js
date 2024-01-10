e5e2f8ea14b1bf6a2bc65b16f92df6eec6a7f432
franklinjunior <franklinjunior021118@gmail.com>
2024-01-10 18:47:20 -0500

    10/01

--- src/main/index.js
+++ src/main/index.js
@@ -3,15 +3,17 @@ import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 import { collectSystemInfo } from './SystemInfo'
+import AutoLaunch from 'auto-launch'
+import { autoUpdater } from 'electron-updater'
+
+let mainWindow
 
 function createWindow() {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 900,
     height: 600,
     show: false,
-    // @Ventana de la applicacion;
-    //frame: false,
     resizable: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
@@ -21,9 +23,11 @@ function createWindow() {
       sandbox: false
     }
   })
+
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
   })
+
   mainWindow.webContents.on('did-finish-load', () => {
     collectSystemInfo()
       .then((data) => {
@@ -39,45 +43,73 @@ function createWindow() {
     return { action: 'deny' }
   })
 
-  // HMR for renderer base on electron-vite cli.
+  // HMR for renderer based on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
+  mainWindow.on('closed', () => {
+    mainWindow = null // Establecer la referencia a null
+  })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// Configuración de autoinicio (AutoLaunch)
+const appLauncher = new AutoLaunch({
+  name: 'agentinventoy',
+  isHidden: false // Mantener oculta la ventana principal
+})
+
+// Configuración de autoactualización (electron-updater)
+autoUpdater.setFeedURL({
+  provider: 'github',
+  owner: 'franklinjunior23',
+  repo: 'AppAgentInti'
+})
+
 app.whenReady().then(() => {
   // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
-  createWindow()
+  // No cierres la aplicación, solo oculta la ventana
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      if (mainWindow) {
+        mainWindow.hide()
+      }
+    }
+  })
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  // Configuración de autoinicio
+  appLauncher
+    .isEnabled()
+    .then((isEnabled) => {
+      if (!isEnabled) {
+        // Habilitar autoinicio si está deshabilitado
+        appLauncher.enable()
+      }
+    })
+    .catch((err) => {
+      console.error('Error al verificar el estado de autoinicio:', err)
+    })
+
+  // Comprobar actualizaciones automáticamente al iniciar la aplicación
+  autoUpdater.checkForUpdates()
+
+  // Crear la ventana principal
+  createWindow()
+  app.on('activate', () => {
+    // Muestra la ventana cuando se activa la aplicación
+    if (mainWindow === null) {
+      createWindow()
+    } else {
+      mainWindow.show()
+    }
   })
 })
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
